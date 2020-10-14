56ec34de6b058d32277e3d58e2b4a1ae2dfa9869
vcc1432 <valerie.gersen@gmail.com>
2020-10-14 23:18:28 +0200

    Responsive styling

--- src/components/Start.js
+++ src/components/Start.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Start = () => {
   return (
-    <div className="center">
+    <div className="center start">
       <h3> Please enter a search term to find a movie's characters </h3>
       <div>
         <p>Or watch Baby Yoda being adorable <a target='_blank' href="https://youtu.be/rXCbf9vdMxM">here!</a> 🥰</p> 
